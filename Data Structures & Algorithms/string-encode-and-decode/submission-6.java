0580e3e47eb7d36d5a8aafaa1e736578d2e676e1
vk3690 <65496270+vk3690@users.noreply.github.com>
2026-06-08 15:07:13 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder stb=new StringBuilder();
+            for(String str:strs)
+            {   
+                stb.append(str.length()).append("-").append(str).append("-");
+            }
+            return stb.toString();
+
+
+    }
+
+    public List<String> decode(String str) {
+
+        List<String> strs=new ArrayList<>();
+        if( str.isEmpty())
+        {
+            return strs;
+        }
+        
+
+            Integer start=str.indexOf("-");
+            Integer lenOfStr=Integer.valueOf((str.substring(0,start)));
+            start++;
+            Integer end=start+lenOfStr;
+        while(!str.isEmpty())
+        {
+
+           String substring=str.substring(start,end);
+            // System.out.println(substring);
+
+           strs.add(substring);
+           str=str.substring(end+1);
+            // System.out.println(str);
+
+            start=str.indexOf("-");
+                        // System.out.println(start);
+
+            if(start>0)
+            {
+             lenOfStr=Integer.valueOf((str.substring(0,start)));
+            start++;
+             end=start+lenOfStr;
+             
+            }else{
+                break;
+            }
+        }
+        
+        return strs;
+
+    }
+}
